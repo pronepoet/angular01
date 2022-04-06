5411eeffc750f15c529df174dcb705dbc936874b
pronepoet <ochiengphillip84@gmail.com>
2022-04-07 01:10:37 +0300

    add a new class and used ngfor directive to loop between goals

--- Ang01/src/app/app.component.ts
+++ Ang01/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ang01';
+  goals:Goal[] = [
+   {id: 1, name: 'Watch finding Nemo'},
+   {id: 2, name: 'Buy Cookies'},
+   {id: 3, name: 'Get new Phone Case'},
+   {id: 4, name: 'Get Dog Food'},
+   {id: 5, name: 'Solve Math HomeWork'},
+   {id: 6, name:  'Plot My World domination plan'}, 
+  ];
 }
+
